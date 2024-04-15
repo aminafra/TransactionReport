bedd2d09121de2145e4230b454a913f01359606a
aminafra <mohamadamin.afrasiabi@gmail.com>
2024-04-16 01:51:21 +0330

    Add Entities into the Domain

---
+++ src/Domain/Entities/Person.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+public class Person : BaseEntity
+{
+    public string Name { get; set; }
+    public string Family { get; set; }
+}
---
+++ src/Domain/Entities/Transaction.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+public class Transaction : BaseEntity
+{
+    public DateTime TransactionDate { get; set; }
+    public decimal Price { get; set; }
+    public int PersonId { get; set; }
+    public Person Person { get; set; }
+}
