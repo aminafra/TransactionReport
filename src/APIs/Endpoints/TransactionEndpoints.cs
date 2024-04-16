3d20255c2b5baa89dffd476849d5944f944623f3
aminafra <mohamadamin.afrasiabi@gmail.com>
2024-04-16 23:05:41 +0330

    Add Endpoints and fix program.cs

---
+++ src/APIs/Endpoints/TransactionEndpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace APIs.Endpoints;
+
+public static class TransactionEndpoints
+{
+    public static void MapTransactionEndpoints(this IEndpointRouteBuilder builder)
+    {
+        builder.MapGet("GetTransactionReport/{userId:int}", (
+            int? userId,
+            CancellationToken ct) =>
+        {
+
+        });
+    }
+}
--- src/APIs/Program.cs
+++ src/APIs/Program.cs
@@ -1,44 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using APIs.Endpoints;
+using Application;
+using Asp.Versioning;
+using Infrastructure;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddEndpointsApiExplorer()
+                    .AddSwaggerGen()
+                    .RegisterApplicationServices()
+                    .RegisterPersistenceServices(builder.Configuration)
+                    .AddApiVersioning(options =>
+                    {
+                        options.DefaultApiVersion = new ApiVersion(1);
+                        options.ReportApiVersions = true;
+                        options.ApiVersionReader = new UrlSegmentApiVersionReader();
+                    })
+                    .AddApiExplorer(options =>
+                    {
+                        options.GroupNameFormat = "'v'V";
+                        options.SubstituteApiVersionInUrl = true;
+                    });
+}
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+    app.UseHttpsRedirection();
 
-app.UseHttpsRedirection();
+    var apiVersionSet = app.NewApiVersionSet()
+        .HasApiVersion(new ApiVersion(1))
+        .ReportApiVersions()
+        .Build();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var versionGroup = app
+        .MapGroup("Api/V{apiVersion:apiVersion}")
+        .WithApiVersionSet(apiVersionSet);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    versionGroup.MapTransactionEndpoints();
+    app.Run();
 }
