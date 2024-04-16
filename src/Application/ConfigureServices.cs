427ac6911ffade08269f8e2530f614178bd811d4
aminafra <mohamadamin.afrasiabi@gmail.com>
2024-04-16 23:03:12 +0330

    Add MediatR Package

---
+++ src/Application/ConfigureServices.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+public static class ConfigureServices
+{
+    public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
