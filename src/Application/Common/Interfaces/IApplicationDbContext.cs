3511e05368fb1016fbf991199db14d52fa236afe
aminafra <mohamadamin.afrasiabi@gmail.com>
2024-04-16 23:02:50 +0330

    use IApplicationDbContext in Application Layer

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces;
+public interface IApplicationDbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
