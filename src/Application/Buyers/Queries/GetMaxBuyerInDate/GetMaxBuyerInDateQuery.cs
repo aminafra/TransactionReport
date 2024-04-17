15549bf4f125cee17a815d6e0abf41f9ba21a0c9
aminafra <mohamadamin.afrasiabi@gmail.com>
2024-04-17 19:35:09 +0330

    add GetMaxBuyerInDateQuery

---
+++ src/Application/Buyers/Queries/GetMaxBuyerInDate/GetMaxBuyerInDateQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.Buyers.Queries.GetMaxBuyer;
+using MediatR;
+using System.Text.Json.Serialization;
+
+namespace Application.Buyers.Queries.GetMaxBuyerInDate;
+public class GetMaxBuyerInDateQuery : IRequest<GetMaxBuyerDto>
+{
+    [JsonPropertyName("start_date")]
+    public DateTime StartDate { get; set; }
+
+    [JsonPropertyName("end_date")]
+    public DateTime EndDate { get; set; }
+}
---
+++ src/Application/Buyers/Queries/GetMaxBuyerInDate/GetMaxBuyerInDateQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Buyers.Queries.GetMaxBuyer;
+using Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Buyers.Queries.GetMaxBuyerInDate;
+public class GetMaxBuyerInDateQueryHandler(IApplicationDbContext context)
+    : IRequestHandler<GetMaxBuyerInDateQuery, GetMaxBuyerDto?>
+{
+    public async Task<GetMaxBuyerDto?> Handle(
+        GetMaxBuyerInDateQuery request,
+        CancellationToken cancellationToken)
+        => await context.Persons
+            .Include(p => p.Transactions)
+            .OrderByDescending(x => x.Transactions
+                .Where(t => t.TransactionDate >= request.StartDate && t.TransactionDate <= request.EndDate)
+                .Sum(t => t.Price))
+            .Select(x => new GetMaxBuyerDto(
+                x.Name,
+                x.Family))
+            .FirstOrDefaultAsync(cancellationToken);
+
+}
